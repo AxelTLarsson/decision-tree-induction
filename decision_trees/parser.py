1fa2d840a5c62fc28d81d7e78bdac450e11d9279
AxelTLarsson <mail@axellarsson.nu>
2016-02-29 16:36:55 +0100

    Add parser impl, correctly parses contact-lenses.arff

--- decision_trees/parser.py
+++ decision_trees/parser.py
@@ -59,13 +59,101 @@ class Lexer:
                                (s[pos], line))
 
 
-if __name__ == '__main__':
-    test = '''
+class Parser:
+    """
+    A simple recursive descent parser for a subset of Weka ARFF.
+    """
 
-    @data
-    4.4,?,1.5,?,Iris-setosa
-    '''
-    f = open('example.arff', 'r')
-    test = f.read()
-    for token in Lexer.tokenize(test):
-        print(token)
+    def __init__(self, token_generator):
+        self.token_generator = token_generator
+        self.current_token = None
+
+    def accept(self, token_type):
+        if self.current_token.typ == token_type:
+            # print("\nAccept {}".format(self.current_token))
+            t = self.current_token
+            self.next_token()
+            return t
+        else:
+            raise RuntimeError(
+                "Cannot acccept {}, current_token: {}".format(
+                    token_type, self.current_token))
+
+    def expect(self, token_type):
+        if self.current_token is None:
+            return False
+        return self.current_token.typ == token_type
+
+    def next_token(self):
+        try:
+            self.current_token = next(self.token_generator)
+        except StopIteration:
+            print('Parsing done')
+            self.current_token = None
+
+    def parse(self):
+        # Init
+        self.next_token()
+
+        # Expecting @relation <string>
+        self.accept("RELATION_DECL")
+        rel = self.accept('STRING')
+        print("Relation is {}".format(rel.value))
+
+        # Expecting a number of @attribute <name> <datatype>
+        # TODO should refactor to new method
+        while self.expect('ATTR_DECL'):
+            self.accept('ATTR_DECL')
+            attr_name = self.accept('STRING')
+            print("@attribute {}".format(attr_name.value))
+            if self.expect('NUM_DATATYPE'):
+                data_type = self.accept('NUM_DATATYPE')
+                print("datatype is {}".format(data_type.value))
+            elif self.expect('LEFT_CURLY'):
+                self.accept('LEFT_CURLY')
+                print(self.nominal_values())
+
+        if self.expect('DATA_DECL'):
+            self.data()
+        else:
+            raise RuntimeError('No DATA section found!')
+
+    def nominal_values(self):
+        """
+        Parse nominal values, i.e. {nom1, nom2, nom3}
+        Expecting that when this method called, curent_token
+        is at nom1.
+        """
+        nominal_values = []
+        nom1 = self.accept('STRING')
+        nominal_values.append(nom1.value)
+        while self.expect('SEMI'):
+            self.accept('SEMI')
+            nomx = self.accept('STRING')
+            nominal_values.append(nomx.value)
+
+        self.accept('RIGHT_CURLY')
+        return nominal_values
+
+    def data(self):
+        """
+        Parse the data section.
+        """
+        self.accept('DATA_DECL')
+
+        # loop through the whole data section
+        while self.expect('STRING'):
+
+            datas = []
+            data1 = self.accept('STRING')
+            datas.append(data1.value)
+            # one line
+            while self.expect('SEMI'):
+                self.accept('SEMI')
+                data = self.accept('STRING')
+                datas.append(data.value)
+            print("Datas: {}".format(datas))
+
+
+if __name__ == '__main__':
+    pass
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-import decision_trees.parser as parser
+from decision_trees.parser import Parser, Lexer
 
 
 class TestParser(TestCase):
@@ -9,10 +9,18 @@ class TestParser(TestCase):
             test = f.read()
             # since tokenize is a generator, we must convert to list
             # to be able to go through it more than once
-            tokens = list(parser.Lexer.tokenize(test))
+            tokens = list(Lexer.tokenize(test))
+            for t in tokens:
+                print(t)
             rels = filter(lambda t: t.typ == 'RELATION_DECL', tokens)
             self.assertEqual(1, len(list(rels)))
             attrs = filter(lambda t: t.typ == 'ATTR_DECL', tokens)
             self.assertEqual(5, len(list(attrs)))
             datas = filter(lambda t: t.typ == 'DATA_DECL', tokens)
             self.assertEqual(1, len(list(datas)))
+
+    def test_the_parser(self):
+        with open('tests/contact-lenses.arff', 'r') as f:
+            s = f.read()
+            parser = Parser(Lexer.tokenize(s))
+            parser.parse()
