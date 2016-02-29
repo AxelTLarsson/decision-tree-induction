27413a0408d6c3750197db4d99f803a5cffcf56c
lb08abu <belcher_lewis@yahoo.com>
2016-02-29 14:40:35 +0100

    Written pseudocode for decision-tree-learning from fig. 18.5

--- decision_trees/tree.py
+++ decision_trees/tree.py
@@ -1,10 +1,25 @@
+"""
+Module containing decision tree induction.
+"""
 
-import numpy as np
 
+def decision_tree_learning(examples, attributes, parent_attributes):
 
-class DecisionTree:
-    def __init__(self):
-        pass
+    if not examples:
+        return plurality_value(parent_attributes)
+    elif examples_have_same_classification(examples):
+        return classification
+    elif not attributes:
+        return plurality_value(examples)
+    else:
+        A = max([importance(a, examples) for a in attributes])
+        tree = new_decision_tree(root_test=A)
+        for vk in A:
+            exs = set()
+            subtree = decision_tree_learning(exs, attributes - A, examples)
+            tree.add_branch(A=vk, subtree=subtree)
+
+    return tree
 
 
 if __name__ == '__main__':
