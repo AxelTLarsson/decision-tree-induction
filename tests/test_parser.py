0a2497a97fb046b38ea373411a3133b2b5f42329
AxelTLarsson <mail@axellarsson.nu>
2016-03-01 16:28:09 +0100

    Move contact-lense.arff to Data

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,7 +5,7 @@ from decision_trees.parser import Parser, Lexer
 class TestParser(TestCase):
 
     def test_the_lexer(self):
-        with open('tests/contact-lenses.arff', 'r') as f:
+        with open('data/contact-lenses.arff', 'r') as f:
             test = f.read()
             # since tokenize is a generator, we must convert to list
             # to be able to go through it more than once
@@ -20,7 +20,7 @@ class TestParser(TestCase):
             self.assertEqual(1, len(list(datas)))
 
     def test_the_parser(self):
-        with open('tests/contact-lenses.arff', 'r') as f:
+        with open('data/contact-lenses.arff', 'r') as f:
             s = f.read()
             parser = Parser(Lexer.tokenize(s))
             data = parser.parse()
